7d542bc2140f3e244313167a629e66ad4e7733ea
Obbycreator rblx <obbycreatorrblx@gmail.com>
2026-04-28 06:22:25 +0200

    ExtendedFinished

--- InfExperimentExtended_1.py
+++ InfExperimentExtended_1.py
@@ -1,30 +1,30 @@
-Streichhölzer = int(input("Wie viele Streichhölzer es zu Beginn geben? "))
-WegznmdHölzer = int(input("Wie viele Streichhlözer darf man auf einmal wegnehmen?"))
+import random
+from time import sleep
 
-_skipPlr1 = False
+Streichhölzer = int(input("Wie viele Streichhölzer soll es zu Beginn geben? "))
+sleep(.25)
+WegznmdHölzer = int(input("Wie viele Streichhlözer darf man auf einmal wegnehmen? "))
+sleep(.75)
+print("Das Spiel beginnt.")
+sleep(.75)
 
 while Streichhölzer > 0:
-    if _skipPlr1 == False:
-        inputplr1 = int(input(f"Übrige Streichhölzer: {Streichhölzer}. Spieler 1, gib eine Zahl zwischen 1 und {WegznmdHölzer} ein:"))
-        if inputplr1 < 0 or inputplr1 > WegznmdHölzer:
-            print("Ungültige Zahleingabe!")
-            continue
-        else:
-            Streichhölzer -= inputplr1
-            if Streichhölzer <= 0:
-                print("Spieler 2 hat gewonnen!")
-                break
-
-
-    if _skipPlr1 == True:
-        _skipPlr1 == False
-    inputplr2 = int(input(f"Übrige Streichhölzer: {Streichhölzer}. Spieler 2, gib eine Zahl zwischen 1 und {WegznmdHölzer} ein:"))
-    if inputplr2 < 0 or inputplr2 > WegznmdHölzer:
+    inputplr1 = int(input(f"Übrige Streichhölzer: {Streichhölzer}. Spieler 1, gib eine Zahl zwischen 1 und {WegznmdHölzer} ein: "))
+    if inputplr1 < 0 or inputplr1 > WegznmdHölzer:
         print("Ungültige Zahleingabe!")
-        _skipPlr1 = True
         continue
     else:
-        Streichhölzer -= inputplr2
+        Streichhölzer -= inputplr1
         if Streichhölzer <= 0:
-            print("Spieler 2 hat gewonnen!")
+            print("Der Roboter gewinnt! ")
             break
+    sleep(.75)
+
+    random_abzug = random.randint(1, WegznmdHölzer)
+    Streichhölzer -= random_abzug
+    print(f"der Roboter hat {random_abzug} Streichhölzer weggenommen. Es bleiben {(Streichhölzer <= 0 and 0) or Streichhölzer} Streichhölzer übrig.")
+    sleep(.75)
+    if Streichhölzer <= 0:
+        print("Spieler hat gewonnen!")
+        break
+    sleep(.4)
